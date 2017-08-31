9904acf53b03d3f20c24df6b332c8c3dad4f1a7e
Happott <jonas.froehlich1@web.de>
2017-08-31 17:18:18 +0200

    My first commit

--- 2nd Try/ViewController.swift
+++ 2nd Try/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var label_obenmitte: UILabel!
+    @IBOutlet weak var label_rechtsunten: UILabel!
+    @IBOutlet weak var label_buttonlooser: UILabel!
+    
+    var tapCount = 0
+
+    @IBAction func button_push(_ sender: Any) {
+
+        
+    }
+    @IBAction func button_rondom(_ sender: Any) {
+        tapCount += 1
+        print(tapCount)
+        
+        if tapCount >= 10{
+            label_buttonlooser.text = "You pushed the Button 10 times!" }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
