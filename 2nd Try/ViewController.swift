41e7aae465ffadc16346fd696dae83f409b20e9b
Happott <jonas.froehlich1@web.de>
2017-09-04 19:11:42 +0200

    Bool hinzugefügt

--- 2nd Try/ViewController.swift
+++ 2nd Try/ViewController.swift
@@ -12,33 +12,51 @@ import UIKit
 class ViewController: UIViewController {
     
     var tapCount = 0
-
+    
     @IBOutlet weak var First_Number: UITextField!
     @IBOutlet weak var Second_Number: UITextField!
     @IBOutlet weak var label_obenmitte: UILabel!
     @IBOutlet weak var Ergebnis: UILabel!
-
+    
     @IBAction func button_push(_ sender: Any) {
         tapCount += 1
         print(tapCount)
+        if tapCount >= 5 {
+            label_obenmitte.text = "NEARLY DONE"
+        }
         if tapCount >= 10 {
             label_obenmitte.text = "You pushed it to the Limit!"
+        }
+        if tapCount >= 30 {
+            label_obenmitte.text = "Overkill!"
         }
     }
     
     @IBAction func Button_Calculate(_ sender: Any) {
+        
+        let addition = true
+        
+        
         print(First_Number.text!)
         print(Second_Number.text!)
-        Ergebnis.text = "Ergebnis = \(Double(First_Number.text!)! + Double(Second_Number.text!)!)"
+        
+        if addition
+        {
+            Ergebnis.text = "Ergebnis = \(Double(First_Number.text!)! + Double(Second_Number.text!)!)"
+        }
+        else
+        {
+            Ergebnis.text = "Ergebnis = \(Double(First_Number.text!)! - Double(Second_Number.text!)!)"
+        }
     }
-
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
